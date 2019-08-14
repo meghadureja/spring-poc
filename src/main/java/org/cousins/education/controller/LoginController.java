280f6fbb75103bccc4f68370ce173c242d3f7c37
Megha Dureja <megha.dureja01@gmail.com>
2019-08-14 18:05:03 +0530

    Add files via upload
    adding first commit for spring mvc and spring security

---
+++ src/main/java/org/cousins/education/controller/LoginController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.cousins.education.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author megha
+ *
+ */
+
+@Controller
+public class LoginController {
+
+//    @GetMapping("/login")
+//    public String login(Model model) {
+////        model.addAttribute("name", name);
+//        return "login";
+//    }
+}
